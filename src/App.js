e9d2ad1c0303e3d9d149360b277b2efb50739712
vennylee <hs421@naver.com>
2022-06-07 01:34:33 +0900

    feat: 회원가입 페이지 bulma 적용 외 스타일링

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+// module
 import { Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+
+// components
 import Signup from "./Components/Signup";
+import Login from "./Components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Signup />}></Route>
-      </Routes>
-    </div>
+    <>
+      <AppStyle className="App">
+        <Routes>
+          <Route path="/" element={<Signup />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </AppStyle>
+    </>
   );
 }
+
+const AppStyle = styled.div`
+  max-width: 350px;
+  margin: 0 auto;
+`;
 
 export default App;
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { db, auth } from "../shared/firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { addDoc, collection } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
   const [inputValue, setInputValue] = useState({
@@ -9,9 +10,14 @@ const Signup = () => {
     user_name: "",
     user_pw: "",
   });
+  let navigate = useNavigate();
+  const emailRef = useRef(null);
+  const nameRef = useRef(null);
+  const pwRef = useRef(null);
+  //   const nameRefCur = nameRef.current.className;
+  //   const pwRefCur = pwRef.current.className;
 
   const { user_id, user_name, user_pw } = inputValue;
-
   // 이메일 유효성 검사
   const emailRegExp =
     /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
@@ -24,55 +30,101 @@ const Signup = () => {
     setInputValue({ ...inputValue, [name]: value });
   };
 
-  //
   const signup = async () => {
     if (emailRegExp.test(user_id)) {
       if (pwRegExp.test(user_pw)) {
+        // auth에 user 등록\
+        alert("회원가입을 축하드립니다 :)");
         const user = await createUserWithEmailAndPassword(
           auth,
           user_id,
           user_pw
         );
+        // firestore에 저장
         const user_data = await addDoc(collection(db, "user-info"), {
           user_id: user_id,
           user_pw: user_pw,
           user_name: user_name,
         });
+        navigate("/login");
       } else {
         alert("8~15자 영문, 숫자를 조합해주세요 :(");
+        pwRef.current.focus();
       }
     } else {
       alert("유효한 이메일 형식이 아닙니다 :(");
+      emailRef.current.focus();
     }
   };
 
   return (
     <div>
-      아이디(이메일) :{" "}
-      <input
-        type="text"
-        name="user_id"
-        value={user_id}
-        placeholder=""
-        onChange={onChange}
-      />
-      이름 :{" "}
-      <input
-        type="text"
-        name="user_name"
-        value={user_name}
-        placeholder=""
-        onChange={onChange}
-      />
-      비밀번호 :{" "}
-      <input
-        type="text"
-        name="user_pw"
-        value={user_pw}
-        placeholder=""
-        onChange={onChange}
-      />
-      <button onClick={signup}>회원가입</button>
+      <div className="field">
+        <label className="label">Email</label>
+        <div className="control">
+          <input
+            className="input"
+            type="text"
+            name="user_id"
+            value={user_id}
+            placeholder="example@email.com"
+            onChange={onChange}
+            ref={emailRef}
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">Name</label>
+        <div className="control has-icons-left has-icons-right">
+          <input
+            className="input"
+            type="text"
+            name="user_name"
+            value={user_name}
+            placeholder="Name"
+            onChange={onChange}
+            ref={nameRef}
+          />
+          <span className="icon is-small is-left">
+            <i className="fas fa-user"></i>
+          </span>
+          <span className="icon is-small is-right">
+            <i className="fas fa-check"></i>
+          </span>
+        </div>
+        {/* <p className="help is-success">This username is available</p> */}
+      </div>
+      <div className="field">
+        <label className="label">Password</label>
+        <div className="control has-icons-left has-icons-right">
+          <input
+            className="input"
+            type="password"
+            name="user_pw"
+            value={user_pw}
+            placeholder="Password"
+            onChange={onChange}
+            ref={pwRef}
+          />
+          <span className="icon is-small is-left">
+            <i className="fas fa-envelope"></i>
+          </span>
+          <span className="icon is-small is-right">
+            <i className="fas fa-exclamation-triangle"></i>
+          </span>
+        </div>
+        {/* <p className="help is-danger">This email is invalid</p> */}
+      </div>
+      <div className="field is-grouped">
+        <div className="control">
+          <button onClick={signup} className="button is-link">
+            Submit
+          </button>
+        </div>
+        <div className="control">
+          <button className="button is-link is-light">Cancel</button>
+        </div>
+      </div>
     </div>
   );
 };
